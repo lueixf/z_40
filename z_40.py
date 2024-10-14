271a5dd7d6e1bea77b5b9b19da1916926f85e394
lueixf <lueixf@tutamail.com>
2024-10-14 13:30:17 +0300

    z_40

---
+++ z_40.py
@@ -0,0 +1,8 @@
+input_data = open('input.txt', 'r') 
+data = input_data.read()
+data = data.split()
+a = int(data[0])
+output_data = open('output.txt','w')
+output_data.write(str (2 ** a))
+input_data.close()
+output_data.close()
